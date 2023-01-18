36a9ba4b05b2d7ee93679c1e6c6d25039ebdb152
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-01-18 19:17:24 +0530

    Create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Artificial intelligence</h1>
+      <p>
+        Artificial intelligence is the simulation of human intelligence
+        processes by machines, especially computer systems. Specific
+        applications of AI include expert systems, natural language processing,
+        speech recognition and machine vision.
+      </p>
+    </div>
+  );
+}
+export default Notes;
